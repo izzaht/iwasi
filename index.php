82c8bfc6d94b0d52f882d219ebb6d08e428170ed
izzaht <123665599+izzaht@users.noreply.github.com>
2023-01-27 02:10:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name+"viewport" content="with=device-width, initial-scale=1.0">
+     <title>Read It</title>
+     <link rel="stylesheet" href="style.css">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+     <section class="header">
+        <nav>
+            <a href="index.php"><img src="logo.png"></a>
+            <div class="nav-links">
+                <ul>
+                    <li><a href="login.php">LOGIN</a></li>
+                    <li><a href="search.php">SEARCH</a></li>
+                    <li><a href="favourite.php">FAVOURITE</a></li>
+                    <li><a href="saved.php">SAVED</a></LI>
+                </ul>
+            </div>
+        </nav>
+
+     <div class="text-box">
+         <h1>SKINCARE TIPS</h1>
+         <p>You can find the tips for your skincare here.</p>
+         <p>Login to save your last read.</p>
+         <a href="book.php"class="hero-btn">Let's solve your problem!</a>
+     </div>    
+    
+     </section>
+
+
+    
+</body>
+</html>    
---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name+"viewport" content="with=device-width, initial-scale=1.0">
+     <title>Read It</title>
+     <link rel="stylesheet" href="style.css">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+     <section class="header">
+        <nav>
+            <a href="index.php"><img src="logo.png"></a>
+            <div class="nav-links">
+                <ul>
+                    <li><a href="login.php">LOGIN</a></li>
+                    <li><a href="search.php">SEARCH</a></li>
+                    <li><a href="favourite.php">FAVOURITE</a></li>
+                    <li><a href="saved.php">SAVED</a></LI>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="loginbox">
+            <img src="images/logo8-removebg-preview.png" class="avatar">
+            <h1>Login Here</h1>
+            <form>
+               <p>Username</p>
+               <input type="text" name="" placeholder="Enter Username">
+               <p>Password</p>
+               <input type="password" name="" placeholder="Enter Password">
+               <input type="Submit" name="" value="Login"><br>
+               
+               <a href="signup.php">Don't have an account?</a><br>
+               <a href="#">Lost your password?</a>
+   
+   
+            </form>
+           
+           </div>  
+    
+     </section>
+
+
+    
+</body>
+</html>    
